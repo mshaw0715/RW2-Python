ddd3b99f2eca3d56beecf0103b1dfb1496f25464
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-04-09 02:23:44 +0000

    welcome box

---
+++ welcome.py
@@ -0,0 +1,18 @@
+# header / footer
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+# starts with a blank line 
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+"""
+this is a python multi line comments
+everything in between the triple quotes will NOT be read
+"""
