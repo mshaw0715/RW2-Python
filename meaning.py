ab2cc240819855523ebc099f986dc5bcc67bcb00
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-04-10 01:09:11 +0000

    meaning.py

---
+++ meaning.py
@@ -0,0 +1,25 @@
+meaning = 20
+print('')
+
+# if meaning > 10:
+#    print('Right on!)
+#else 
+#    print('Not today')
+
+#ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
+
+
+x = 10
+y = 20
+z = 30
+
+if x > 5 and y > 10:
+    print("Both are True")
+else:
+    print("At least 1 is not true")
+
+if x > 5 or y > 10:
+    print("At least 1 is true")
+else:
+    print("None are true")
