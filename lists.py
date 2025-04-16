8be35b098fd1183a7168ae1164ae9863963fb0cf
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-04-16 01:41:43 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,31 @@
+users = ['Ashley', 'Michael', 'Drew']
+
+data = ['Ashley', 25, True]
+
+emptylist = []
+
+# print("Is Ashley in users?")
+
+print("Ashley" in users)
+print("Ashley" in data)
+print("Ashley" in emptylist)
+
+print(users[0])
+
+
+print(users[-1])
+print(users[-2])
+
+print(users.index('Michael'))
+
+print(users[0:])
+
+print(len(users))
+
+users.append('Judy')
+print(users)
+
+
+users.insert(0, 'Gurney')
+print(users)
+
