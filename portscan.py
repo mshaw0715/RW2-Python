8c6a5f01067a0a0bfaa61a5c526924c6cdc693c5
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-04-18 01:03:24 +0000

    port scan is done

--- portscan.py
+++ portscan.py
@@ -1,8 +1,30 @@
 # imports
-import time, sys
+import time, sys, os, datetime, socket
 
 def scan():
-    
+    os.system("clear")
+    print("\n\n*** Python TCP Port Scanner ***\n\n")
+
+    host = input("Enter the Host IP address: ")
+    sport = int(input("Enter starting Port #: "))
+    eport = int(input("Enter ending Port #: "))
+
+    st = datetime.datetime.now().strftime("%a %b %d %Y, %I:%M:%S %p")
+    print(f"\nStarted port scan on {host} at {st}...")
+
+    for port in range(sport, eport + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.3)
+
+        result = sock.connect_ex((host, port))
+
+        if result == 0:
+            print(f"Port {port} is open")
+
+        sock.close
+
+    ft = datetime.datetime.now().strftime("%a %b %d %Y, %I:%M:%S %p")
+    print(f"\nPort scan completed at {ft}.")
 
 # loop controls program flow
 answer = "yes"
