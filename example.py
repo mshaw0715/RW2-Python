7c6d8799df3ca0a0fadb054b8032c04371a5a135
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-04-24 01:36:00 +0000

    examples and recursion

---
+++ example.py
@@ -0,0 +1,18 @@
+
+# value = True
+
+# while value:
+#     print(value)
+#     value = False
+
+value = "y"
+count = 0
+
+while value:
+    count += 1
+    print(count)
+    if (count == 5):
+        break
+    else:
+        value = 0
+        continue
---
+++ recursion.py
@@ -0,0 +1,16 @@
+
+def add_one(num):
+
+    if (num >= 9):
+        return num + 1
+    
+    total = num + 1
+    print(total)
+
+    return add_one(total)
+
+# my_val = add_one(0)
+
+add_one(0)
+
+# print(f"my value is: {my_val}")
