5842edb543a1fc57fa3fa9cd80ea827ec7e29e8e
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-05-01 01:34:17 +0000

    hello persons

---
+++ hello_person.py
@@ -0,0 +1,34 @@
+# command line arguments are like poor mans SSH
+# when yo see words spaced apart, that is a command
+
+def hello(name, lang):
+    greetings = {
+        "English": "Hello",
+        "Spanish": "Hola",
+        "German": "Hallo",
+    }
+
+msg = f""
+print(msg)
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Provides a personal greeting."
+)
+
+parser.add_argument(
+    "-n", "--name", metavar="name",
+    required=True, help="The name of the person to greet."
+)
+
+parser.add_argument(
+    "-l", "--lang", metevar="language", required=True,
+    choices=["English","Spanish","German"]
+
+)
+args = parser.parse_args()
+
+mesg = f"Hello {args.name}!"
+print(mesg)
+
