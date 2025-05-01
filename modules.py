ecfeb93624cb1a38055e25a79293f862377f59dc
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-05-01 00:31:32 +0000

    modules

--- modules.py
+++ modules.py
@@ -10,3 +10,18 @@ print(f"the valuable of my variable is {value}")
 import math
 
 print(math.pi)
+
+import sys
+# import random
+
+# random.choice("123")
+
+from enum import Enum
+
+import random as rdm
+
+for item in dir(rdm):
+    print(item)
+
+print(dir(rdm))
+
