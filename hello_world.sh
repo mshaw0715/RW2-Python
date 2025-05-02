8b6558d772a258f87deb896f28d80b4c53969594
Mary Ashley Shaw <ashaw41799@yahoo.com>
2025-05-02 00:53:53 +0000

    bash

--- hello_world.sh
+++ hello_world.sh
@@ -7,5 +7,15 @@ mesg="Hello World!"
 echo $mesg
 
 # print variable to the screen 2nd method
-echo ${mesg}
+# echo ${mesg}
 
+echo "Who do you want to say hello to?"
+
+read person
+
+echo Hello ${person}
+
+# for x in {1..25}; do echo "I have ${x} dollars in my pocket"; sleep 2; done
+# for x in {1..10}; do echo "interface ${x}"; echo "switch port access vlan 10"; done
+
+echo $1
